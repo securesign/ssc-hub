db7b3cd7cbcf7b83ef125ee15f4aa6c4c6289888
kahboom <rachelyordan@gmail.com>
2024-07-30 14:13:29 +0100

    task: setup astro

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: 'My Docs',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        slug: 'guides/example'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  })],
+  output: "server",
+  adapter: netlify()
+});
